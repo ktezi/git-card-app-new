b7b6faaac08edda2db63813ee5944035681c88ef
Tezi <tezikhan1@gmail.com>
2020-05-18 14:56:46 +0530

    Updates

---
+++ precache-manifest.73b85c4fcc9c490d5453ac8d00745284.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb2d9ea6c4cac873d1e773d33107bbc2",
+    "url": "/git-card-app-new/index.html"
+  },
+  {
+    "revision": "06b57c8df2a1e2b97c18",
+    "url": "/git-card-app-new/static/css/main.93ee0a37.chunk.css"
+  },
+  {
+    "revision": "c2fe6ba115868292b91e",
+    "url": "/git-card-app-new/static/js/2.fea522c3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/git-card-app-new/static/js/2.fea522c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06b57c8df2a1e2b97c18",
+    "url": "/git-card-app-new/static/js/main.cf2c94a7.chunk.js"
+  },
+  {
+    "revision": "a11090ac7822fc06546a",
+    "url": "/git-card-app-new/static/js/runtime-main.d3f4fd87.js"
+  }
+]);
